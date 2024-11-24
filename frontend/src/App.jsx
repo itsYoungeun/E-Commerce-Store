ed1fb4f567145d99b84f76d1ed6334c986d21b3f
Peter <youngeunpc@yahoo.com>
2024-11-24 16:43:11 -0300

    Frontend file setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   )
 }
 
