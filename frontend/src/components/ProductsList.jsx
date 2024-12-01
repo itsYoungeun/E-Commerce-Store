082bef72935d14f3be16d1ae3ffebc30896577f0
Peter <youngeunpc@yahoo.com>
2024-11-30 21:22:07 -0300

    Products admin dashboard

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,7 +1,14 @@
+import { motion } from "framer-motion";
+import { Trash, Star } from "lucide-react";
+import { useProductStore } from "../stores/useProductStore";
+
 const ProductsList = () => {
+  const { deleteProduct, toggleFeaturedProduct, products } = useProductStore();
+  console.log("products", products);
+
   return (
     <div>ProductsList</div>
   )
 }
 
-export default ProductsList
+export default ProductsList;
